e9ac1867cefa0be7fe2ac9fb3096f0b1f5396ddb
gusgatam <gustavo.gatamora@gmail.com>
2020-12-04 16:00:53 -0300

    Devolución.
    Coemntarios en el primer ejercicio, verificar y subir trabajos adeudados.

--- Guia Clase 13/Ejercicio 1.c
+++ Guia Clase 13/Ejercicio 1.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-int a,RT=0,R[]{1,2,3,4,5};
-for (a=0;a<5;a++){
+//Falta función.
+int main()
+{
+int a,RT=0,R[5]{0};
+for (a=0;a<5;a++)
+{
 printf("Ingrese las resistencias ");
 scanf("%d",&R[a]);}
-for (a=0;a<5;a++){
+for (a=0;a<5;a++)
+{
 printf("\n Las resistencias son %d",R[a]);
 RT=RT+R[a];
 }
